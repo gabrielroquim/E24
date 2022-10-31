1b5a2f6b7c3f0a83e010c2cbdd0d648b023bd1ac
Gabriel Roquim <gabrielroquim@hotmail.com>
2022-10-31 19:03:51 -0300

    primeiros testes superteting

--- sample-supertest-api-testing-main/test/adresses.spec.js
+++ sample-supertest-api-testing-main/test/adresses.spec.js
@@ -12,26 +12,26 @@ describe('Adresses', () => {
         token = await getAccessToken('gabriel', 'admin')
     })
 
-    it('(AdressesRegister) Cadastrar Endereços', async() => {
-        let address1 = `${faker.address.streetAddress()}`  
-        let address2 = `${faker.address.secondaryAddress()}` 
-        let city =  `${faker.address.city()}`  
-        let state = `${faker.address.state()}` 
-       // let zip = `${faker.address.zipCode()}` 
+    it('(AdressesRegister) Cadastrar Endereços', async () => {
+        let address1 = `${faker.address.streetAddress()}`
+        let address2 = `${faker.address.secondaryAddress()}`
+        let city = `${faker.address.city()}`
+        let state = `${faker.address.state()}`
+        // let zip = `${faker.address.zipCode()}` 
 
         await req(API_URL)
-        .post('/addresses')
-        .set('Accept', 'application/json')
-        .set('Authorization', `Bearer ${token}`)
- 
-       .send( {
-            "address_1":address1,
-            "address_2": address2,
-            "city": city,
-            "state":state,
-            "zip": 0,
-          })
-          
+            .post('/addresses')
+            .set('Accept', 'application/json')
+            .set('Authorization', `Bearer ${token}`)
+
+            .send({
+                "address_1": address1,
+                "address_2": address2,
+                "city": city,
+                "state": state,
+                "zip": 0,
+            })
+
     });
     it('(AdressesCheck) Listar Endereços', async () => {
         await req(API_URL)
@@ -40,16 +40,9 @@ describe('Adresses', () => {
             .set('Authorization', `Bearer ${token}`)
             .set('Acept', 'aplication/json')
             .then(response => {
-                expect(response.statusCode).toEqual(200)             
+                expect(response.statusCode).toEqual(200)
                 expect(response.body.id).not.toBe(toString)
-                expect(response.body).toBeInstanceOf(Array)        
-             
-              
-                
-                               
+                expect(response.body).toBeInstanceOf(Array)
             })
     });
-
-
-
 });
