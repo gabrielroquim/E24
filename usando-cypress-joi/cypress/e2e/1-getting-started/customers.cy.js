ce61fc0529ec64c1923d61f6814b770092366d7f
Gabriel Roquim <gabrielroquim@hotmail.com>
2022-11-07 19:47:58 -0300

    instalação do faker-br

--- usando-cypress-joi/cypress/e2e/1-getting-started/customers.cy.js
+++ usando-cypress-joi/cypress/e2e/1-getting-started/customers.cy.js
@@ -1,21 +1,34 @@
 /// <reference types="cypress" />
+var faker = require('faker-br')
 
 describe('Teste de Cliente API', () => {
+
+    cy.request({
+        method: 'POST',
+        url: '/customers',
+        body: {
+
+            "address": {
+                "id": "cl9xciy570133bwpikkxw5vyv"
+            },
+            "email": "amanda@cypress.com",
+            "firstName": "Amanda",
+            "lastName": "Cypress",
+            "phone": "01198530447"
+        }
+    })
 
     it.only(' Deve Listar Clientes', () => {
         cy.request({
             method: 'GET',
-            url: 'http://localhost:3000/api/customers',
-            body: {
-                "username": "gabriel",
-                "password": "admin"
-            }
+            url: '/customers',
+
         }).then((response) => {
-            expect(response.status).to.equal(200)
+            expect(response.status).to.equal(201)
             expect(response.body.message).to.equal('Login realizado com sucesso')
             expect(response.data).to.have.property('email')
-           
-        })
-    });
 
-});
+        })
+    })
+
+})
